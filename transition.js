4e675517a2306c44c61847a64e4fbf6f7c63903d
Yiduo Ke <yiduoke822@jhs185.org>
2018-03-24 00:23:05 -0400

    DONEEEE

--- transition.js
+++ transition.js
@@ -1,9 +1,49 @@
-var one = [2.99, 3.54, -0.552, 16.5]//2016
+var one = [995, 888, 516, 484, 185, 159];//2016 FY 2016 Agency Spending
 //total revenues, total spending, total surplus/deficit, GDP
-var two = [2.96, 3.36, -0.399, 16.2]//2015
+var two = [935, 859, 512, 442, 176, 145];//2015
+// Dept of Health and Human Services
+// Social Security Administration
+// Dept of Defense
+// Dept of the Treasury
+// Other
+// Dept of Veterans Affairs
 
 var chart = d3.select(".chart");
 var bar = chart.selectAll("div");
-var barUpdate1 = bar.data(one);
-var barUpdate2 = bar.data(two);
-var barEnter = barUpdate.enter().append("div");
+
+var button = document.getElementById("other");
+var chart_DOM = document.getElementById("chart_DOM");
+var heading = document.getElementById("heading");
+var year = -1; //-1 for 2016 and 1 for 2015
+
+var new_chart = function(){
+    if (year < 0){//2016
+        var barUpdate = bar.data(one);
+        var barEnter = barUpdate.enter().append("div");
+        barEnter.transition().duration(1500).style("width", function(d) {
+            return d / 2 + "px"; });
+    }
+    else{//2015
+        var barUpdate = bar.data(two);
+        var barEnter = barUpdate.enter().append("div");
+        barEnter.transition().duration(1500).style("width", function(d) {
+            return d / 2 + "px"; });
+    }
+
+    barEnter.text(function(d) { return d; });
+}
+
+new_chart();
+
+var other = function(){
+    chart_DOM.innerHTML = "";
+    year *= -1;
+    if (year < 0){
+        heading.innerHTML = "Spending by Department by Billions in 2016";
+    }
+    else{
+        heading.innerHTML = "Spending by Department by Billions in 2015";
+    }
+    new_chart();
+}
+button.addEventListener("click", other);
